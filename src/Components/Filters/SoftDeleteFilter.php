a30ea2455ce0e20472afbb3a3b21adf5e147049a
Craig Smith <craig.smith@customd.com>
2022-07-01 16:00:59 +1200

    fix: defer to core version

--- src/Components/Filters/SoftDeleteFilter.php
+++ src/Components/Filters/SoftDeleteFilter.php
@@ -3,24 +3,8 @@
 namespace Phpsa\FilamentCms\Components\Filters;
 
 use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Filters\TrashedFilter;
 
-class SoftDeleteFilter
+class SoftDeleteFilter extends TrashedFilter
 {
-    public static function make(): SelectFilter
-    {
-        return SelectFilter::make('deleted_at')
-            ->options([
-                'withoutTrashed' => __('filament-cms::filament-cms.table.filter.active'),
-                'onlyTrashed'    => __('filament-cms::filament-cms.table.filter.deleted'),
-            ])
-            ->label(__('filament-cms::filament-cms.table.filter.trashed'))
-            ->default('without-trashed')
-            ->query(
-                fn ($builder, $filter) => match ($filter['value']) {
-                    'withoutTrashed' => $builder->withoutTrashed(),
-                    'onlyTrashed'    => $builder->onlyTrashed(),
-                    default    => $builder->withTrashed(),
-                }
-            );
-    }
 }
